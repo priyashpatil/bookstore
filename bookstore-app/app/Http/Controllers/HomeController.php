ea41b2b7c3cbf6d71a2ea280146f2539212285b1
Priyash <38959321+priyashpatil@users.noreply.github.com>
2023-11-29 18:23:42 +0530

    add scout search on books

--- bookstore-app/app/Http/Controllers/HomeController.php
+++ bookstore-app/app/Http/Controllers/HomeController.php
@@ -12,10 +12,7 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
-        $books = Book::when($request->has('q'), function ($q) use ($request) {
-            $query = $request->query('q');
-            $q->where('title', 'like', "%$query%");
-        })->latest()->paginate();
+        $books = Book::search($request->query('q'))->paginate();
 
         return view('welcome', compact('books'));
     }
