bc16f16f9fbb40fd7aa41bc003a8ab7cb8c28245
Priyash <38959321+priyashpatil@users.noreply.github.com>
2023-11-29 18:50:35 +0530

    disable all links and clean up dark mode selector

--- bookstore-app/resources/views/admin/includes/sidebar.blade.php
+++ bookstore-app/resources/views/admin/includes/sidebar.blade.php
@@ -9,7 +9,7 @@
         <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+                    <a class="nav-link disabled d-flex align-items-center gap-2 active" aria-current="page" href="#">
                         <svg class="bi">
                             <use xlink:href="#house-fill"/>
                         </svg>
@@ -17,7 +17,15 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link disabled d-flex align-items-center gap-2" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
+                            <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
+                        </svg>
+                        Books
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled d-flex align-items-center gap-2" href="#">
                         <svg class="bi">
                             <use xlink:href="#file-earmark"/>
                         </svg>
@@ -25,7 +33,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link disabled d-flex align-items-center gap-2" href="#">
                         <svg class="bi">
                             <use xlink:href="#cart"/>
                         </svg>
@@ -33,7 +41,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link disabled d-flex align-items-center gap-2" href="#">
                         <svg class="bi">
                             <use xlink:href="#people"/>
                         </svg>
@@ -41,7 +49,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link disabled d-flex align-items-center gap-2" href="#">
                         <svg class="bi">
                             <use xlink:href="#graph-up"/>
                         </svg>
@@ -49,7 +57,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link disabled d-flex align-items-center gap-2" href="#">
                         <svg class="bi">
                             <use xlink:href="#puzzle"/>
                         </svg>
@@ -68,7 +76,7 @@
             </h6>
             <ul class="nav flex-column mb-auto">
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link disabled d-flex align-items-center gap-2" href="#">
                         <svg class="bi">
                             <use xlink:href="#file-earmark-text"/>
                         </svg>
@@ -76,7 +84,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link disabled d-flex align-items-center gap-2" href="#">
                         <svg class="bi">
                             <use xlink:href="#file-earmark-text"/>
                         </svg>
@@ -84,7 +92,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link disabled d-flex align-items-center gap-2" href="#">
                         <svg class="bi">
                             <use xlink:href="#file-earmark-text"/>
                         </svg>
@@ -92,7 +100,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link disabled d-flex align-items-center gap-2" href="#">
                         <svg class="bi">
                             <use xlink:href="#file-earmark-text"/>
                         </svg>
@@ -105,7 +113,7 @@
 
             <ul class="nav flex-column mb-auto">
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link disabled d-flex align-items-center gap-2" href="#">
                         <svg class="bi">
                             <use xlink:href="#gear-wide-connected"/>
                         </svg>
@@ -113,7 +121,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link disabled d-flex align-items-center gap-2" href="#">
                         <svg class="bi">
                             <use xlink:href="#door-closed"/>
                         </svg>
