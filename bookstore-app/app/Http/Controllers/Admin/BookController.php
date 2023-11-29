32148fb89925803cacfe640afe0cb87121bc4299
Priyash <38959321+priyashpatil@users.noreply.github.com>
2023-11-29 19:28:39 +0530

    add edit

--- bookstore-app/app/Http/Controllers/Admin/BookController.php
+++ bookstore-app/app/Http/Controllers/Admin/BookController.php
@@ -37,9 +37,9 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        return view('admin.books.show', compact('book'));
     }
 
     /**
---
+++ bookstore-app/resources/views/admin/books/show.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container py-3" style="max-width: 720px;">
+
+        <div class="card mb-3">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img class="img-fluid" src="{{ $book->image }}" alt="{{ $book->title }}">
+                    </div>
+                    <div class="col-md-8">
+                        <h1 class="h2">{{ $book->title }}</h1>
+                        <div><b>Author:</b> {{ $book->author }}</div>
+                        <div><b>Genre:</b> {{ $book->genre }}</div>
+                        <div><b>Published:</b> {{$book->published->format('d-m-Y')}}</div>
+                        <div><b>Publisher:</b> {{$book->publisher}}</div>
+                        <div><b>ISBN:</b> {{$book->isbn}}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <h2 class="h4">Description</h2>
+                <div>{{$book->desc}}</div>
+            </div>
+        </div>
+
+    </div>
+@endsection
