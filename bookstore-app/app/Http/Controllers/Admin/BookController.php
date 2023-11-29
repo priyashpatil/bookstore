2f274f72e9bc3c7ccffaabe4d9b385cfc5899127
Priyash <38959321+priyashpatil@users.noreply.github.com>
2023-11-29 19:08:34 +0530

    add admin books index and search

--- bookstore-app/app/Http/Controllers/Admin/BookController.php
+++ bookstore-app/app/Http/Controllers/Admin/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
@@ -10,9 +11,11 @@ class BookController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.books.index');
+        $books = Book::search($request->query('q'))->paginate();
+
+        return view('admin.books.index', compact('books'));
     }
 
     /**
--- bookstore-app/resources/views/admin/books/index.blade.php
+++ bookstore-app/resources/views/admin/books/index.blade.php
@@ -1,5 +1,44 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1 class="pt-3 h2">Books</h1>
+
+    <div class="pt-3 mb-3 d-flex align-items-center justify-content-between">
+        <h1 class="h2">Books</h1>
+        <form>
+            <input type="search" name="q" class="form-control" placeholder="Search" aria-label="Search"
+                   value="{{request()->query('q')}}">
+        </form>
+    </div>
+
+    <div class="mb-3">{{ $books->links() }}</div>
+
+    <div class="table-responsive small">
+        <table class="table table-striped table-sm">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Author</th>
+                <th scope="col">Published</th>
+                <th scope="col">Genre</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            @foreach($books as $book)
+                <tr>
+                    <td>{{$book->isbn}}</td>
+                    <td><a href="{{route('admin.books.show', $book->id)}}">{{$book->title}}</a></td>
+                    <td>{{$book->author}}</td>
+                    <td>{{$book->published->format('d-m-Y')}}</td>
+                    <td>{{$book->genre}}</td>
+                </tr>
+            @endforeach
+
+            </tbody>
+        </table>
+    </div>
+
+    <div class="mt-3">{{ $books->links() }}</div>
+
 @endsection
