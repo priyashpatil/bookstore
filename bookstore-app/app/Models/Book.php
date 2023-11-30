dec38449182b4b7ebf29db45c96a5ffbd4db6b00
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2023-11-30 06:55:55 +0530

    add soft delete support

--- bookstore-app/app/Models/Book.php
+++ bookstore-app/app/Models/Book.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Scout\Searchable;
 
 class Book extends Model
 {
     use Searchable;
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = ['title', 'author', 'genre', 'publisher', 'isbn', 'published', 'desc', 'image'];
 
--- bookstore-app/database/migrations/2023_11_29_105429_create_books_table.php
+++ bookstore-app/database/migrations/2023_11_29_105429_create_books_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->string('publisher');
             $table->text('desc');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
