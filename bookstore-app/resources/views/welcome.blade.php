e2a01e3b7b81b4f7ccecf4e4cabc095b503fd83a
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2023-11-30 23:37:56 +0530

    add results not found placeholder

--- bookstore-app/resources/views/welcome.blade.php
+++ bookstore-app/resources/views/welcome.blade.php
@@ -13,53 +13,61 @@
             <input v-model="query" name="q" class="form-control me-2" type="search"
                    placeholder="Search by title, author, isbn, genre" v-on:input="meta.current_page=1; search();"
                    aria-label="Search by title, author, isbn, genre" value="{{request()->query('q')}}">
-{{--            <button class="btn btn-outline-success" type="submit">Search</button>--}}
+            {{--            <button class="btn btn-outline-success" type="submit">Search</button>--}}
         </form>
 
-        <nav class="mt-b" aria-label="Page navigation">
-            <ul class="pagination">
-                <li class="page-item" :class="{ disabled: !link.url, active: link.active }"
-                    v-for="(link, index) in meta.links" :key="index">
-                    <a class="page-link" href="#" v-html="link.label" v-on:click="changePage(link)"></a>
-                </li>
-            </ul>
-        </nav>
+        <div class="text-center" v-if="books.length === 0">
+            <h2 class="h3">Oops! We looks like there's a typo.</h2>
+            <p>Try searching with other keyword</p>
+        </div>
 
-        <div class="card mb-2" v-for="book in books">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-3">
-                        <img class="img-fluid" :src="book.image" :alt="book.title">
-                    </div>
-                    <div class="col-md-9">
-                        <h3><a :href="'/' + book.id">@{{ book.title }}</a></h3>
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <div><b>Author:</b> @{{ book.author }}</div>
-                                <div><b>Genre:</b> @{{ book.genre }}</div>
-                                <div><b>Published:</b> @{{book.published}}</div>
-                            </div>
-                            <div class="col-md-6">
-                                <div><b>Publisher:</b> @{{book.publisher}}</div>
-                                <div><b>ISBN:</b> @{{book.isbn}}</div>
-                                <div><b>Price:</b> ₹@{{book.sale_price}}
-                                    <del>@{{book.price}}</del>
+        <div v-if="books.length !== 0">
+            <nav class="mt-b" aria-label="Page navigation">
+                <ul class="pagination">
+                    <li class="page-item" :class="{ disabled: !link.url, active: link.active }"
+                        v-for="(link, index) in meta.links" :key="index">
+                        <a class="page-link" href="#" v-html="link.label" v-on:click="changePage(link)"></a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div class="card mb-2" v-for="book in books">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img class="img-fluid" :src="book.image" :alt="book.title">
+                        </div>
+                        <div class="col-md-9">
+                            <h3><a :href="'/' + book.id">@{{ book.title }}</a></h3>
+                            <div class="row mb-3">
+                                <div class="col-md-6">
+                                    <div><b>Author:</b> @{{ book.author }}</div>
+                                    <div><b>Genre:</b> @{{ book.genre }}</div>
+                                    <div><b>Published:</b> @{{book.published}}</div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div><b>Publisher:</b> @{{book.publisher}}</div>
+                                    <div><b>ISBN:</b> @{{book.isbn}}</div>
+                                    <div><b>Price:</b> ₹@{{book.sale_price}}
+                                        <del>@{{book.price}}</del>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <nav class="mt-3" aria-label="Page navigation">
+                <ul class="pagination">
+                    <li class="page-item" :class="{ disabled: !link.url, active: link.active }"
+                        v-for="(link, index) in meta.links" :key="index">
+                        <a class="page-link" href="#" v-html="link.label" v-on:click="changePage(link)"></a>
+                    </li>
+                </ul>
+            </nav>
         </div>
 
-        <nav class="mt-3" aria-label="Page navigation">
-            <ul class="pagination">
-                <li class="page-item" :class="{ disabled: !link.url, active: link.active }"
-                    v-for="(link, index) in meta.links" :key="index">
-                    <a class="page-link" href="#" v-html="link.label" v-on:click="changePage(link)"></a>
-                </li>
-            </ul>
-        </nav>
     </div>
 @endsection
 
