7782734cb22c67b10a0d565e54801fdfd5342002
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2023-11-30 23:50:15 +0530

    add throttling requests

--- bookstore-app/resources/views/welcome.blade.php
+++ bookstore-app/resources/views/welcome.blade.php
@@ -16,56 +16,64 @@
             {{--            <button class="btn btn-outline-success" type="submit">Search</button>--}}
         </form>
 
-        <div class="text-center" v-if="books.length === 0">
-            <h2 class="h3">Oops! We looks like there's a typo.</h2>
-            <p>Try searching with other keyword</p>
+        <div class="text-center" v-if="loading">
+            <div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
         </div>
 
-        <div v-if="books.length !== 0">
-            <nav class="mt-b" aria-label="Page navigation">
-                <ul class="pagination">
-                    <li class="page-item" :class="{ disabled: !link.url, active: link.active }"
-                        v-for="(link, index) in meta.links" :key="index">
-                        <a class="page-link" href="#" v-html="link.label" v-on:click="changePage(link)"></a>
-                    </li>
-                </ul>
-            </nav>
+        <div id="booksContainer" class="d-none">
+            <div class="text-center" v-if="books.length === 0 && loading === false">
+                <h2 class="h3">Oops! We looks like there's a typo.</h2>
+                <p>Try searching with other keyword</p>
+            </div>
 
-            <div class="card mb-2" v-for="book in books">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img class="img-fluid" :src="book.image" :alt="book.title">
-                        </div>
-                        <div class="col-md-9">
-                            <h3><a :href="'/' + book.id">@{{ book.title }}</a></h3>
-                            <div class="row mb-3">
-                                <div class="col-md-6">
-                                    <div><b>Author:</b> @{{ book.author }}</div>
-                                    <div><b>Genre:</b> @{{ book.genre }}</div>
-                                    <div><b>Published:</b> @{{book.published}}</div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div><b>Publisher:</b> @{{book.publisher}}</div>
-                                    <div><b>ISBN:</b> @{{book.isbn}}</div>
-                                    <div><b>Price:</b> ₹@{{book.sale_price}}
-                                        <del>@{{book.price}}</del>
+            <div v-if="books.length !== 0">
+                <nav class="mt-b" aria-label="Page navigation">
+                    <ul class="pagination">
+                        <li class="page-item" :class="{ disabled: !link.url, active: link.active }"
+                            v-for="(link, index) in meta.links" :key="index">
+                            <a class="page-link" href="#" v-html="link.label" v-on:click="changePage(link)"></a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div class="card mb-2" v-for="book in books">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img class="img-fluid" :src="book.image" :alt="book.title">
+                            </div>
+                            <div class="col-md-9">
+                                <h3><a :href="'/' + book.id">@{{ book.title }}</a></h3>
+                                <div class="row mb-3">
+                                    <div class="col-md-6">
+                                        <div><b>Author:</b> @{{ book.author }}</div>
+                                        <div><b>Genre:</b> @{{ book.genre }}</div>
+                                        <div><b>Published:</b> @{{book.published}}</div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div><b>Publisher:</b> @{{book.publisher}}</div>
+                                        <div><b>ISBN:</b> @{{book.isbn}}</div>
+                                        <div><b>Price:</b> ₹@{{book.sale_price}}
+                                            <del>@{{book.price}}</del>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <nav class="mt-3" aria-label="Page navigation">
-                <ul class="pagination">
-                    <li class="page-item" :class="{ disabled: !link.url, active: link.active }"
-                        v-for="(link, index) in meta.links" :key="index">
-                        <a class="page-link" href="#" v-html="link.label" v-on:click="changePage(link)"></a>
-                    </li>
-                </ul>
-            </nav>
+                <nav class="mt-3" aria-label="Page navigation">
+                    <ul class="pagination">
+                        <li class="page-item" :class="{ disabled: !link.url, active: link.active }"
+                            v-for="(link, index) in meta.links" :key="index">
+                            <a class="page-link" href="#" v-html="link.label" v-on:click="changePage(link)"></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
 
     </div>
@@ -75,14 +83,16 @@
     <script
         src="{{\Illuminate\Support\Facades\App::environment('production') ? 'https://unpkg.com/vue@3/dist/vue.global.prod.js' : 'https://unpkg.com/vue@3/dist/vue.global.js' }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios@1.6.2/dist/axios.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js"></script>
 
-    <script type="text/javascript">
+    <script type="text/javascript" defer>
         const {createApp} = Vue;
         const searchEndpoint = 'http://localhost/api/books';
 
         createApp({
             data() {
                 return {
+                    loading: false,
                     query: '',
                     books: [],
                     meta: {
@@ -96,13 +106,14 @@
                 }
             },
             methods: {
-                search: function () {
+                search: _.throttle(function () {
                     // Prep URL.
                     let url = new URL(searchEndpoint);
                     url.searchParams.append('q', this.query);
                     url.searchParams.append('page', this.meta.current_page);
 
                     // Execute API call.
+                    this.loading = true;
                     axios.get(url).then((res) => {
                         if (res.status === 200) {
                             this.books = res.data.data;
@@ -115,10 +126,10 @@
                         alert('Something went wrong.');
                         console.error(e);
                     }).finally(() => {
-                        console.log('Data loading completed.');
+                        this.loading = false;
+                        // console.log('Data loading completed.');
                     })
-                },
-
+                }, 1000),
                 changePage(link) {
                     if (link.url) {
                         let url = new URL(link.url);
@@ -129,6 +140,7 @@
             },
             mounted() {
                 this.search();
+                document.querySelector('#booksContainer').classList.remove('d-none');
             }
         }).mount('#app')
     </script>
