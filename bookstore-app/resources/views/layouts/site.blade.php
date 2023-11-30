c21a69cdcaac80bba96bf9b42a6aa3098560e24f
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2023-11-30 22:57:59 +0530

    remove search from nav

--- bookstore-app/resources/views/layouts/site.blade.php
+++ bookstore-app/resources/views/layouts/site.blade.php
@@ -10,25 +10,9 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom">
     <div class="container" style="max-width: 720px;">
-        <a class="navbar-brand" href="{{route('home')}}">BookStore</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('home')}}">Home</a>
-                </li>
-            </ul>
-            <form class="d-flex" role="search">
-                <input name="q" class="form-control me-2" type="search" placeholder="Search by title, author, isbn"
-                       aria-label="Search by title, author, isbn" value="{{request()->query('q')}}">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-        </div>
+        <a class="navbar-brand fw-semibold" href="{{route('home')}}">BookStore</a>
     </div>
 </nav>
 
